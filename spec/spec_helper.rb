b27195c7b8e799de9c6744a9b5f0f93f3c5d66b5
Julian Fiander <julian.fiander@mangolanguages.com>
2017-11-16 18:00:45 -0500

    Clear out specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,25 +4,22 @@ require 'simplecov'
 SimpleCov.start
 
 require 'usps_flags'
-require 'usps_flags/burgees'
+require 'usps_flags/grades'
 
-# The spec for USPSFlags::Burgees contains an example that checks for
-# USPSFlags::Errors::UnknownBurgee
+# The spec for USPSFlags::Grades contains an example that checks for
+# USPSFlags::Errors::InvalidGrade
 RSpec::Expectations.configuration.on_potential_false_positives = :nothing
 
 RSpec.configure do |config|
   config.before(:suite) do
     $tmp_flags_dir = "tmp/flags"
-    $tmp_burgees_dir = "tmp/path/to/burgees"
 
     USPSFlags.configure do |c|
       c.flags_dir = $tmp_flags_dir
-      c.burgees_dir = $tmp_burgees_dir
     end
   end
 
   config.after(:suite) do
     ::FileUtils.rm_rf("tmp") if ::Dir.exist?("tmp")
-    ::FileUtils.rm_rf(USPSFlags.configuration.burgees_dir) if ::Dir.exist?(USPSFlags.configuration.burgees_dir)
   end
 end
---
+++ spec/usps_flags/grades_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe USPSFlags::Grades do
+  pending
+end
